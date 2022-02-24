aa76de880a7f542ff35b6b628700b5a65051f386
MisterMAX789 <maximotruc@mail.ru>
2022-02-25 02:12:51 +0300

    add test

---
+++ src/test/java/CardApplication.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardApplication {
+    @Test
+     void shouldFillLines () {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Иванов Владислав");
+        form.$("[data-test-id=phone] input").setValue("+79997257198");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type= button").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+}
